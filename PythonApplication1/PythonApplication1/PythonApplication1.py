b751d78c5bd36fd96351d4039f3903707fbd58a4
bobbedhair <yeaeun23@naver.com>
2015-09-21 14:16:01 +0900

    0902 실습
    0902 실습

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,30 @@
+﻿#print('Hello World')
+#print('I have {0:d} cats'.format(6))
+
+'''
+print("한글")
+
+salary=int(input('Please enter your salary: '))
+bonus=int(input('Please enter your bonus: '))
+payCheck=salary+bonus
+#payCheck=float(salary)+float(bonus)
+print(payCheck)
+
+print(type(payCheck))
+'''
+
+#name="greenjoa"
+#print(name[0])
+#print(name[-2])
+
+#info="201311190kang"
+#sid=info[:9]
+#sname=info[9:]
+#print(sid+" "+sname)
+#print(sname[0:2])
+
+#a="I eat %-10s apples%%."%"five"
+#print(a)
+
+message=input("종료할까요? ")
+print(message.lower())
